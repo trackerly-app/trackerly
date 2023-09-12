659d9e093367485bbdbeeedfc53ac3eb676b2129
Darren Pavel <darren.pavel@gmail.com>
2023-09-12 16:24:39 -0700

    Squashed commit of the following:
    commit 3f25e0de4cbc2a2a4a405ab880e106a4c4a56e22
    Author: Darren Pavel <darren.pavel@gmail.com>
    Date:   Tue Sep 12 15:02:46 2023 -0700
    
        Moved ApplicationForm from containers->components
    
    commit d1c489205c5c8589fc4030402e612148dc2b4237
    Author: Darren Pavel <darren.pavel@gmail.com>
    Date:   Tue Sep 12 14:46:48 2023 -0700
    
        Added basic ApplicationForm tests
        Added configurations for testing next.js

---
+++ client/src/app/containers/CompanyForm.tsx
@@ -0,0 +1,10 @@
+export type CompanyProps = {
+  user_id: number;
+  id?: number;
+};
+
+const CompanyForm = (props: CompanyProps) => {
+  return <div></div>;
+};
+
+export default CompanyForm;
--- client/src/jest.config.js
+++
@@ -1,9 +0,0 @@
-import { nextJest } from 'next/jest';
-const createJestConfig = nextJest({
-  dir: './',
-});
-const customJestConfig = {
-  moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jest-environment-jsdom',
-};
-module.exports = createJestConfig(customJestConfig);
