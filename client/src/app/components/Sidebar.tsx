22d0e09570250a64fac3a880c32ffaa7cedd0413
Darren Pavel <darren.pavel@gmail.com>
2023-09-12 20:48:18 -0700

    Squashed commit of the following:
    commit cec1799be4deecf55937bf13913e70d328c6b902
    Merge: 5614a2d 607628d
    Author: Darren Pavel <darren.pavel@gmail.com>
    Date:   Tue Sep 12 20:46:48 2023 -0700
    
        Merge branch 'dev' of https://github.com/trackerly-app/trackerly into dev
    
    commit fe855f140f7767b7d5dde362749c1f2ba8a8aea6
    Author: Victor Martins <martinsvictor287@gmail.com>
    Date:   Wed Sep 13 03:20:11 2023 +0300
    
        add dashboard refactor
    
    commit 5ce9949ea9e328b182ba45e06ef204522d3ac64a
    Merge: 1565476 15f3a26
    Author: Darren Pavel <darren.pavel@gmail.com>
    Date:   Tue Sep 12 17:14:50 2023 -0700
    
        Merge branch 'dev' of https://github.com/trackerly-app/trackerly into dev
    
    commit d69851daef8e5f16b7b804a9376dd8a6dc7a6fdb
    Author: Victor Martins <martinsvictor287@gmail.com>
    Date:   Wed Sep 13 03:04:53 2023 +0300
    
        add login and signup form

--- client/src/app/components/Sidebar.tsx
+++ client/src/app/components/Sidebar.tsx
@@ -22,8 +22,8 @@ const Sidebar = () => {
           xmlns='http://www.w3.org/2000/svg'
         >
           <path
-            clipRule='evenodd'
-            fillRule='evenodd'
+            clip-rule='evenodd'
+            fill-rule='evenodd'
             d='M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z'
           ></path>
         </svg>
@@ -34,7 +34,7 @@ const Sidebar = () => {
         className='fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0'
         aria-label='Sidebar'
       >
-        <div className='h-full px-3 py-4 overflow-y-auto'>
+        <div className='h-full px-3 py-4 overflow-y-auto  bg-violet-600 text-white'>
           <div className='flex items-end gap-5'>
             <Image
               src={logo}
@@ -52,10 +52,10 @@ const Sidebar = () => {
             <li>
               <a
                 href='#'
-                className='flex items-center p-2 text-gray-900 rounded-lg'
+                className='flex items-center p-2 rounded-lg'
               >
                 <svg
-                  className='w-5 h-5 text-gray-500 transition duration-75'
+                  className='w-5 h-5 transition duration-75'
                   aria-hidden='true'
                   xmlns='http://www.w3.org/2000/svg'
                   fill='currentColor'
@@ -72,10 +72,10 @@ const Sidebar = () => {
             <li>
               <a
                 href='#'
-                className='flex items-center p-2 text-gray-900 rounded-lg'
+                className='flex items-center p-2 rounded-lg'
               >
                 <svg
-                  className='w-5 h-5 text-gray-500 transition duration-75'
+                  className='w-5 h-5 transition duration-75'
                   aria-hidden='true'
                   xmlns='http://www.w3.org/2000/svg'
                   fill='currentColor'
@@ -91,8 +91,8 @@ const Sidebar = () => {
         </div>
       </aside>
 
-      <div className='p-4 sm:ml-64'>
-        <div className='p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700'>
+      <div className='p-4 sm:ml-64 h-100'>
+        <div className='p-4 border-2 border-gray-200 border-dashed rounded-lg'>
           <div className='grid grid-cols-3 gap-4 mb-4'>
             <div className='flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800'>
               <p className='text-2xl text-gray-400 dark:text-gray-500'>
@@ -105,9 +105,9 @@ const Sidebar = () => {
                 >
                   <path
                     stroke='currentColor'
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    strokeWidth='2'
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                    stroke-width='2'
                     d='M9 1v16M1 9h16'
                   />
                 </svg>
@@ -170,181 +170,6 @@ const Sidebar = () => {
                 />
               </svg>
             </p>
-          </div>
-          <div className='grid grid-cols-2 gap-4 mb-4'>
-            <div className='flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800'>
-              <p className='text-2xl text-gray-400 dark:text-gray-500'>
-                <svg
-                  className='w-3.5 h-3.5'
-                  aria-hidden='true'
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 18 18'
-                >
-                  <path
-                    stroke='currentColor'
-                    stroke-linecap='round'
-                    stroke-linejoin='round'
-                    stroke-width='2'
-                    d='M9 1v16M1 9h16'
-                  />
-                </svg>
-              </p>
-            </div>
-            <div className='flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800'>
-              <p className='text-2xl text-gray-400 dark:text-gray-500'>
-                <svg
-                  className='w-3.5 h-3.5'
-                  aria-hidden='true'
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 18 18'
-                >
-                  <path
-                    stroke='currentColor'
-                    stroke-linecap='round'
-                    stroke-linejoin='round'
-                    stroke-width='2'
-                    d='M9 1v16M1 9h16'
-                  />
-                </svg>
-              </p>
-            </div>
-            <div className='flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800'>
-              <p className='text-2xl text-gray-400 dark:text-gray-500'>
-                <svg
-                  className='w-3.5 h-3.5'
-                  aria-hidden='true'
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 18 18'
-                >
-                  <path
-                    stroke='currentColor'
-                    stroke-linecap='round'
-                    stroke-linejoin='round'
-                    stroke-width='2'
-                    d='M9 1v16M1 9h16'
-                  />
-                </svg>
-              </p>
-            </div>
-            <div className='flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800'>
-              <p className='text-2xl text-gray-400 dark:text-gray-500'>
-                <svg
-                  className='w-3.5 h-3.5'
-                  aria-hidden='true'
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 18 18'
-                >
-                  <path
-                    stroke='currentColor'
-                    stroke-linecap='round'
-                    stroke-linejoin='round'
-                    stroke-width='2'
-                    d='M9 1v16M1 9h16'
-                  />
-                </svg>
-              </p>
-            </div>
-          </div>
-          <div className='flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800'>
-            <p className='text-2xl text-gray-400 dark:text-gray-500'>
-              <svg
-                className='w-3.5 h-3.5'
-                aria-hidden='true'
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 18 18'
-              >
-                <path
-                  stroke='currentColor'
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
-                  stroke-width='2'
-                  d='M9 1v16M1 9h16'
-                />
-              </svg>
-            </p>
-          </div>
-          <div className='grid grid-cols-2 gap-4'>
-            <div className='flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800'>
-              <p className='text-2xl text-gray-400 dark:text-gray-500'>
-                <svg
-                  className='w-3.5 h-3.5'
-                  aria-hidden='true'
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 18 18'
-                >
-                  <path
-                    stroke='currentColor'
-                    stroke-linecap='round'
-                    stroke-linejoin='round'
-                    stroke-width='2'
-                    d='M9 1v16M1 9h16'
-                  />
-                </svg>
-              </p>
-            </div>
-            <div className='flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800'>
-              <p className='text-2xl text-gray-400 dark:text-gray-500'>
-                <svg
-                  className='w-3.5 h-3.5'
-                  aria-hidden='true'
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 18 18'
-                >
-                  <path
-                    stroke='currentColor'
-                    stroke-linecap='round'
-                    stroke-linejoin='round'
-                    stroke-width='2'
-                    d='M9 1v16M1 9h16'
-                  />
-                </svg>
-              </p>
-            </div>
-            <div className='flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800'>
-              <p className='text-2xl text-gray-400 dark:text-gray-500'>
-                <svg
-                  className='w-3.5 h-3.5'
-                  aria-hidden='true'
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 18 18'
-                >
-                  <path
-                    stroke='currentColor'
-                    stroke-linecap='round'
-                    stroke-linejoin='round'
-                    stroke-width='2'
-                    d='M9 1v16M1 9h16'
-                  />
-                </svg>
-              </p>
-            </div>
-            <div className='flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800'>
-              <p className='text-2xl text-gray-400 dark:text-gray-500'>
-                <svg
-                  className='w-3.5 h-3.5'
-                  aria-hidden='true'
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 18 18'
-                >
-                  <path
-                    stroke='currentColor'
-                    stroke-linecap='round'
-                    stroke-linejoin='round'
-                    stroke-width='2'
-                    d='M9 1v16M1 9h16'
-                  />
-                </svg>
-              </p>
-            </div>
           </div>
         </div>
       </div>
